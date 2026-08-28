5ef9a0711b639573e3ee3f5aab8b5df91074525f
Dhanush S <dhanushgowdas222@gmail.com>
2026-08-28 21:51:58 +0530

    Add DS18B20 temperature sensor test code
    Initialize DS18B20 temperature sensor and print temperature readings.

--- Firmware/DS18B20 Temperature_test_code.ino
+++ Firmware/DS18B20 Temperature_test_code.ino
@@ -1,1 +1,41 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define TEMP_PIN 4
+
+OneWire oneWire(TEMP_PIN);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  sensors.begin();
+
+  Serial.println("DS18B20 Temperature Sensor Test");
+
+  Serial.print("Sensors found: ");
+  Serial.println(sensors.getDeviceCount());
+}
+
+void loop() {
+
+  sensors.requestTemperatures();
+
+  float temperature =
+      sensors.getTempCByIndex(0);
+
+  Serial.print("Temperature: ");
+
+  if (temperature == DEVICE_DISCONNECTED_C) {
+
+    Serial.println("SENSOR DISCONNECTED");
+
+  } else {
+
+    Serial.print(temperature, 2);
+    Serial.println(" °C");
+  }
+
+  delay(1000);
+}
